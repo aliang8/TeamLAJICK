69ed8cf89a63aa32c89ad928638fc9b3dc067df7
Issac Kim <ikim1@stuy.edu>
2017-01-19 14:50:58 -0500

    work on store js

--- static/js/custom.js
+++ static/js/custom.js
@@ -29,3 +29,33 @@ $(document).ready(function(){
         $("#myCarousel").carousel("next");
     });
 });
+
+
+var addReward = function(e){
+    var li = document.createElement("li");
+    //var name = prompt("Reward Name:");
+    /var cost = prompt("Reward Value:");
+    //var button = "<button id=\"obtain\">purchase</button>";
+    //button.addEventListener("click", purchase(cost));
+    //button.addEventListener("click", deleteItem);
+    li.innerHTML = "Reward: " //+ name + ", Costs: " + cost + button
+    li.addEventListener("mouseover", headerChange);
+    li.addEventListener("mouseout", restoreHead);
+    var ol = document.getElementById("rewards");
+    ol.appendChild(li);
+}
+
+var deleteReward = function(e){
+    var li.reward =this;
+    this.remove();
+}
+
+
+var listItems = document.getElementsByTagName("li");
+
+for (i = 0; i < listItems.length; i++){
+    listItems[i].addEventListener("click", deleteItem);
+}
+
+var reward_button = document.getElementById("add_reward");
+reward_button.addEventListener("click", addReward);
