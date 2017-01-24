ac324fef6a3da41b86c2ff536836429a4438a246
Issac-Kim <ikim1@stuy.edu>
2017-01-24 15:19:44 -0500

    get reward removing to work

--- static/js/custom.js
+++ static/js/custom.js
@@ -3,6 +3,9 @@ $('#myTabs a').click(function (e) {
   $(this).tab('show');
 })
 
+var removeReward = function(e){
+    this.parentNode.remove();
+}
 
 var addReward = function(e){
     var li = document.createElement("li");
@@ -11,11 +14,16 @@ var addReward = function(e){
     name = name.value;
     var price = document.getElementById("reward_cost");
     price = price.value;
-    li.innerHTML = "Reward: " + name + " Price: " + price + "<button id='purchase'>hello</button>";
+    li.innerHTML = "Reward: " + name + " Price: " + price + "<button class='purchase'>Purchase</button>";
     var ol = document.getElementById("rewards");
     ol.appendChild(li);
+    var reward_list = document.getElementById("rewards");
+    var rewards = reward_list.getElementsByClassName("purchase");
+    for(i=0; i < rewards.length; i++){
+	rewards[i].addEventListener("click", removeReward);
+
+    }
+
 }
-
-
 var reward_button = document.getElementById("add_reward");
 reward_button.addEventListener("click", addReward);
